60bee933eda31452cbd0ed66b90799df584e9e3e
Truong Le Nguyen (Lazada eLogistics Vietnam) <truong.lenguyen@lazada.vn>
2017-10-11 19:52:22 +0700

    init commit

---
+++ modules/test.py
@@ -0,0 +1,8 @@
+a = 'a\nb\nc'
+
+print(a, end='\r', flush=True)
+
+print(a, end='\r', flush=True)
+
+while True:
+    pass
--- modules/tms.py
+++ modules/tms.py
@@ -5,12 +5,16 @@ import postgres
 from usrlib import query_data
 
 class observer(threading.Thread):
-    observer_list = {}
+    observer_list = []
     def __init__(self):
         threading.Thread.__init__(self)
 
     def register(self, target):
-        
+        self.observer_list.insert(target)
+
+    def print_out(self):
+        print_list = [x.result for x in self.observer_list]
+        print(print_list, end='\r', flush=True)
 
 class singlePipeline(threading.Thread):
     """ Inherit threading to spawn pulling job """
@@ -20,6 +24,7 @@ class singlePipeline(threading.Thread):
         self.postgresdb = postgresdb
         self.job = job
         self.sleep_counter = 0
+        self.result = ''
 
     def run(self):
         pass
